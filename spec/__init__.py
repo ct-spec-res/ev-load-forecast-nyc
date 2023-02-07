1148d9088473c789996eff0cc072477b97cb3138
Kim Sha <kimsha@dhcp-vl2051-1866.eduroam.cornell.edu>
2023-02-07 15:19:50 -0500

    handoff initial eda to ajb

--- spec/__init__.py
+++ spec/__init__.py
@@ -4,6 +4,8 @@ from tqdm.notebook import tqdm
 tqdm.pandas()
 import numpy as np
 import pandas as pd
+pd.set_option("display.max_columns", 1000)
+
 import geopandas as gpd
 import plotly.express as px
 import seaborn as sns
