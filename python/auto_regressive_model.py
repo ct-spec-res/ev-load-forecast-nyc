ad7cf71705faf883cb7278e1860c754914d222f9
Matthew Shen <78379726+MatthewDShen@users.noreply.github.com>
2023-02-08 12:52:26 -0500

    last push to merge ar models

--- python/auto_regressive_model.py
+++ python/auto_regressive_model.py
@@ -1,6 +1,19 @@
+# Import Libraries
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from statsmodels.tsa.ar_model import AutoReg
+
+k_val = 48
 
 
-
+# Create a dataframe with the start time, end time, and vehicle counts
+df = pd.DataFrame({
+    'start_time': pd.date_range(start= '1/1/2020', periods= k_val, freq= '30T'),
+    'end_time': pd.date_range(start= '1/1/2020', periods= k_val, freq= '30T') + dt.timedelta(minutes=30),
+    'vehicle_count': np.random.randint(1800,size=k_val)
+})
 
 df['x_vec'] = np.sqrt(df['vehicle_count'] + 0.25)
 
